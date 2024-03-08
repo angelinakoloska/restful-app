fb7828ff1784c2656d0f147e23e93c1ecf15e5a7
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-08 08:32:44 +0100

    Add models and services

---
+++ models/school.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, Sequelize) => {
+    const School = sequelize.define('School', {
+        Name: Sequelize.DataTypes.STRING,
+        Address: Sequelize.DataTypes.STRING,
+        Description: Sequelize.DataTypes.STRING,
+    },{
+        timestamps: false
+    });
+    School.associate = function(models) {
+        School.hasMany(models.Student);
+        School.hasMany(models.Teacher);
+    };
+    return School
+}
